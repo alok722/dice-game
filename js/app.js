1d1534e6171b7e3edda27e4dafbfdf106f5a983d
Alok Raj <alokr417@gmail.com>
2020-01-03 22:54:31 +0530

    Modualarized

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         //2. Display the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        diceDOM.src = './images/dice-' + dice + '.png';
 
 
         //3. Update the round score IF the rolled number was NOT a 1
